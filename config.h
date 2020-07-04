71fb78bc637508749b159def0c138d5dc4483672
matibbb232 <67790227+matibbb232@users.noreply.github.com>
2020-07-04 07:40:24 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -11,12 +11,13 @@ static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#689d6a";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray4[]       = "#fabd2f";
+static const char col_gray5[]       = "#3c3836";
 static const char col_cyan[]        = "#d65d0e";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	//[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray5,  col_cyan  },
 };
 
 /* tagging */
